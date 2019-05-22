96f8811a51c78fe8b11471155be35be15d3c296e
LimBo90 <abdallah.abdoooo@gmail.com>
2019-05-22 14:23:22 +0200

    setPortValue

--- ARM System/01-MCAL/01-MDIO/MDIO_program.c
+++ ARM System/01-MCAL/01-MDIO/MDIO_program.c
@@ -4,6 +4,30 @@
 #include "MDIO_interface.h"
 #include "MDIO_private.h"
 
+static u64 MLocal_u64ExpandMask(u16 mask){
+	u64 res = 0;
+	u64 mask64 = mask;
+	res |= (mask64 << 0) & (u64)1;
+	res |= (mask64 << 3) & (u64)1 << 4;
+	res |= (mask64 << 6) & (u64)1 << 8;
+	res |= (mask64 << 9) & (u64)1 << 12;
+	res |= (mask64 << 12) & (u64)1 << 16;
+	res |= (mask64 << 15) & (u64)1 << 20;
+	res |= (mask64 << 18) & (u64)1 << 24;
+	res |= (mask64 << 21) & (u64)1 << 28;
+	res |= (mask64 << 24) & (u64)1 << 32;
+	res |= (mask64 << 27) & (u64)1 << 36;
+	res |= (mask64 << 30) & (u64)1 << 40;
+	res |= (mask64 << 33) & (u64)1 << 44;
+	res |= (mask64 << 36) & (u64)1 << 48;
+	res |= (mask64 << 39) & (u64)1 << 52;
+	res |= (mask64 << 42) & (u64)1 << 56;
+	res |= (mask64 << 45) & (u64)1 << 60;
+	res |= res << 1 | res << 2 | res << 3;
+	return res;
+}
+
+
 void MDIO_voidInit(void){
 //	MDIO_GPIOC->CR = 0x2222222200000000;
 //	MDIO_GPIOA->CR = 0x0002000000000000;
@@ -84,3 +108,12 @@ void MDIO_voidSetPinMode(u8 Copy_u8Port, u8 Copy_u8Pin, u8 Copy_PinMode){
 	}
 }
 
+void MDIO_voidSetPortValue(u8 Copy_u8Port, u16 Copy_u8Mask, u16 Copy_u8Value){
+	switch(Copy_u8Port){
+	case MDIO_PORTA:	MDIO_GPIOA->ODR = (MDIO_GPIOA->ODR & ~Copy_u8Mask) | (Copy_u8Value & Copy_u8Mask);	break;
+	case MDIO_PORTB:	MDIO_GPIOB->ODR = (MDIO_GPIOB->ODR & ~Copy_u8Mask) | (Copy_u8Value & Copy_u8Mask);	break;
+	case MDIO_PORTC:	MDIO_GPIOC->ODR = (MDIO_GPIOC->ODR & ~Copy_u8Mask) | (Copy_u8Value & Copy_u8Mask);	break;
+	default:/* Report Error */																				break;
+
+	}
+}
