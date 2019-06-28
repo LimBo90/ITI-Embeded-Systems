a5b834357d8c6beaa30551dd177b3e894ed4325f
LimBo90 <abdullah.abdelrazik@gmail.com>
2019-06-28 23:29:40 +0200

    Implemented my own busy loop delay

--- AVR System/lib/LDELAY.c
+++ AVR System/lib/LDELAY.c
@@ -1,12 +1,26 @@
-#include "LSTD_TYPES.h"
-#include "LUTILS.h"
+/*
+ * LDelay.c
+ *
+ *  Created on: Jun 28, 2019
+ *      Author: Abdelrahman
+ */
 
-#define ONE_LOOP_TIME_ns 133
-void LUTILS_voidDelay_ms(u16 delay_ms){
-	u64 value =  delay_ms * 1000000;
-    u64 nLoops = value / ONE_LOOP_TIME_ns;
-    u64 i;
-	for(i=nLoops; i > 0; i--)
-		asm("NOP");
-	return;
-}
+#include "LSTD_TYPES.h"
+#include "LDelay.h"
+
+#define NLOOPS_IN_1ms ((F_CPU) / 4e3)
+
+
+
+ void LDelay_ms(u16 Copy_u16Time_ms){
+	while(Copy_u16Time_ms > 0){
+		u16 val = NLOOPS_IN_1ms;
+		__asm__ volatile (
+				"1: sbiw %0,1" "\n\t"
+				"brne 1b"
+				: "=w" (val)
+				: "0" (val)
+			);
+		Copy_u16Time_ms--;
+	}
+ }
---
+++ AVR System/lib/LDelay.h
@@ -0,0 +1,15 @@
+/*
+ * LDelay.h
+ *
+ *  Created on: Jun 28, 2019
+ *      Author: Abdelrahman
+ */
+
+#ifndef LDELAY_H_
+#define LDELAY_H_
+
+#define F_CPU 8000000UL
+
+ void LDelay_ms(u16 Copy_u16Time_ms);
+
+#endif /* LDELAY_H_ */
