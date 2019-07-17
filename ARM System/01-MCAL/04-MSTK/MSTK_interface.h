2ccbe7d00ab2db930432a788c699b69d57afc0c9
LimBo90 <abdullah.abdelrazik@gmail.com>
2019-07-18 00:21:07 +0200

    added start() in systick interface

--- ARM System/01-MCAL/04-MSTK/MSTK_interface.h
+++ ARM System/01-MCAL/04-MSTK/MSTK_interface.h
@@ -1,10 +1,12 @@
 #ifndef _MSTK_INTERFACE_H_
 #define _MSTK_INTERFACE_H_
 
-void MSTK_voidInit();
+void MSTK_voidInit(void);
 
 void MSTK_voidSetLoadValue(u32 value);
 
 void MSTK_voidSetCallback(void (* callback) (void));
+
+void MSTK_voidStart(void);
 
 #endif
